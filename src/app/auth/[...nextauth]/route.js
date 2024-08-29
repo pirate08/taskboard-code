4719881ba1acaaca0c5f7687878ca147ea64e66f
Mayukh <mayukhdebgoswami82@gmail.com>
2024-08-29 20:41:43 +0530

    Base Code is ready in dev...

---
+++ src/app/auth/[...nextauth]/route.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const route = () => {
+  return <div># NextAuth.js authentication handlers</div>;
+};
+
+export default route;
---
+++ src/app/boards/[boardId]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>Individual board page component</div>;
+};
+
+export default page;
